2b9051793d30ecc79ee12e322b4966141fa139ba
tuka-p <otsuka.169@gmail.com>
2020-02-24 02:07:11 +0900

    rename point.txt change path.

--- burger_war/scripts/old/point_run.py
+++ burger_war/scripts/old/point_run.py
@@ -20,7 +20,7 @@ class OnigiriRun(object):
 	#ROS subscriber
 	self.move_base_status_sub = rospy.Subscriber('/move_base/status', GoalStatusArray, self.GoalStatusArrayCallback, queue_size=1)
 
-	f=open(os.path.expanduser('~')+'/catkin_ws/src/burger_war/burger_war/scripts/old/point_list.txt')
+	f=open(os.path.expanduser('~')+'/catkin_ws/src/burger_war/burger_navigation/maps/point_list.txt')
 	point_str = f.read()
 	f.close()
 	self.point = []
